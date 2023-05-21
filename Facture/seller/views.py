40b8b201356b6588528b825dc43757134c302f71
Ayk Eminyan <ayk.eminyan@pricehubble.com>
2023-05-22 01:44:14 +0400

    version 0.3.2 small update in views.py

--- Facture/seller/views.py
+++ Facture/seller/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.http import HttpResponse, JsonResponse
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
@@ -87,127 +87,8 @@ class InternalDetailListView(APIView):
 class InternalDetailListFilterView(generics.ListAPIView):
     queryset = Internal.objects.all()
     serializer_class = InternalSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'total_payment', 'total_tax', 'total_payment_after_tax']
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
 
     def get(self, request, *args, **kwargs):
         return Response(self.list(request, *args, **kwargs).data['results'])
-
-#
-# def post(request, *args, **kwargs):
-#     if request.method == 'POST':
-#         name = request.POST['name']
-#         destination = request.POST['destination']
-#         reference = request.POST['reference']
-#         net_a_payer = request.POST['net_a_payer']
-#         quantity = request.POST['quantity']
-#         percent = request.POST['percent']
-#         quantity_after_percent = request.POST['quantity_after_percent']
-#         total_payment = request.POST['total_payment']
-#         total_tax = request.POST['total_tax']
-#         total_payment_after_tax = request.POST['total_payment_after_tax']
-#
-#         if not quantity_after_percent:
-#             quantity_after_percent = int(quantity) * float(percent)
-#         if not quantity:
-#             quantity = round(float(quantity_after_percent) / float(percent), 2)
-#         if not percent:
-#             percent = round(float(quantity_after_percent) / int(quantity), 2)
-#
-#         new_item = Internal(
-#             name=name,
-#             destination=destination,
-#             reference=reference,
-#             net_a_payer=net_a_payer,
-#             quantity=quantity,
-#             percent=percent,
-#             quantity_after_percent=quantity_after_percent,
-#             total_payment=total_payment,
-#             total_tax=total_tax,
-#             total_payment_after_tax=total_payment_after_tax,
-#         )
-#         new_item.save()
-#         form = [
-#             {
-#                 'name': name,
-#                 'destination': destination,
-#                 'net_a_payer': net_a_payer,
-#                 'quantity': quantity,
-#                 'percent': percent,
-#                 'quantity_after_percent': quantity_after_percent,
-#                 'total_payment': total_payment,
-#                 'total_tax': total_tax,
-#                 'total_payment_after_tax': total_payment_after_tax,
-#             }
-#         ]
-#
-#         return ExcelResponse(data=form, output_filename=f'Facture {new_item.created_date}')
-#
-#
-# class InternalCreateData(CreateView):
-#     model = Internal
-#     template_name = 'create_data.html'
-#     fields = [
-#         'id',
-#         'name',
-#         'reference',
-#         'destination',
-#         'quantity',
-#         'percent',
-#         'quantity_after_percent',
-#         'net_a_payer',
-#         'advance_payment',
-#         'total_payment',
-#         'total_tax',
-#         'total_payment_after_tax',
-#     ]
-#
-#     success_message = 'successfully created'
-#
-#     def get_success_url(self):
-#         return reverse('seller:detail', kwargs={'pk': self.object.pk})
-#
-#     def post(self, request, **kwargs):
-#         return post(request)
-#
-#
-# class InternalListData(ListView):
-#     model = Internal
-#     template_name = 'list_data.html'
-#     queryset = model.objects.order_by('-id')
-#
-#
-# class InternalDetailView(DetailView):
-#     model = Internal
-#     template_name = 'detail.html'
-#
-#
-# class InternalDetailUpdate(UpdateView):
-#     model = Internal
-#     template_name = 'update.html'
-#     fields = [
-#         'id',
-#         'name',
-#         'reference',
-#         'destination',
-#         'quantity',
-#         'percent',
-#         'quantity_after_percent',
-#         'net_a_payer',
-#         'advance_payment',
-#         'total_payment',
-#         'total_tax',
-#         'total_payment_after_tax',
-#     ]
-#
-#     def post(self, request, **kwargs):
-#         return post(request)
-#
-#     def get_success_url(self):
-#         return reverse('seller:detail', kwargs={'pk': self.object.pk})
-#
-#
-# class InternalDetailDelete(DeleteView):
-#     model = Internal
-#     template_name = 'delete.html'
-#     success_url = '/'
